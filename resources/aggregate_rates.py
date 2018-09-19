18be99635b19449dbf88de8eb856ac0a67ee7426
Chari <charir2@fr-w-s092346.nih.gov>
2018-09-19 13:05:41 -0400

    Changed aggregate to output samples with low coverage

--- resources/aggregate_rates.py
+++ resources/aggregate_rates.py
@@ -19,7 +19,7 @@ from collections import defaultdict
 from collections import Counter
 from operator import itemgetter
 
-def aggregate_files(input_file_list,modality,output_file):
+def aggregate_files(input_file_list,modality,low_coverage_file,output_file):
 	if modality=='BE4' or modality=='ABE7.10':
 		output_file.write('File\tGene\tTotalCount\tPos1\tPos2\tPos3\tPos4\tPos5\tPos6\tPos7\tPos8\tPos9\tPos10\tPos11\tPos12\tPos13\tPos14\tPos15\tPos16\tPos17\tPos18\tPos19\tPos20\n')
 	else:
@@ -29,18 +29,34 @@ def aggregate_files(input_file_list,modality,output_file):
 		lineCount = 0
 		for line in ifile:
 			if lineCount==1:
-				output_file.write(line)
+				# split the line
+				line = line.rstrip('\r\n')
+				parts = line.split('\t')
+				if modality=='BE4' or modality=='ABE7.10':
+					depth = parts[2]
+				else:
+					depth = parts[3]
+				if int(depth) >= 100:
+					output_file.write(line + '\n')
+				else:
+					head, tail = os.path.split(parts[0])
+					# remove the mpileup_tab
+					tail = tail.replace('_mpileup.tab','')
+					low_coverage_file.write(tail + '\n')
+
 			lineCount += 1
 		ifile.close()
 	output_file.close()
+	low_coverage_file.close()
 
 def main(argv):
 	parser = argparse.ArgumentParser(description=__doc__)
 	parser.add_argument('-i','--input_file_list',nargs='+',required=True)
 	parser.add_argument('-o','--output_file',type=argparse.FileType('w'),required=True)
+	parser.add_argument('-l','--low_coverage_file',type=argparse.FileType('w'),required=True)
 	parser.add_argument('-m','--modality',required=True)
 	opts = parser.parse_args(argv)
-	aggregate_files (opts.input_file_list,opts.modality,opts.output_file)
+	aggregate_files (opts.input_file_list,opts.modality,opts.low_coverage_file,opts.output_file)
  
 if __name__ == '__main__':
 	main(sys.argv[1:])
