226c17596fddd149f1a194db5d970ef648e74858
Chari <charir2@fr-w-s092346.ncifcrf.gov>
2018-10-17 16:16:17 -0400

    Fixed calulate mutation SAM script to include SNPs and replacements

--- resources/calculate_mutation_SAM.py
+++ resources/calculate_mutation_SAM.py
@@ -21,21 +21,30 @@ from collections import defaultdict
 from collections import Counter
 from operator import itemgetter
 
-def process_cigar(cigar_tuple):
+def process_cigar(cigar_tuple,md_tag,aligned_pairs):
 	alterations = []
 	pos = 1
-	for entry in cigar_tuple:
-		# match
-		if int(entry[0])==0:
-			pos = pos + int(entry[1])
-		elif int(entry[0])==1:
-			alt = str(pos) + ':' + str(pos) + ':Ins'
-			alterations.append(alt)
-		elif int(entry[0])==2:
-			end_pos = pos + int(entry[1])
-			alt = str(pos) + ':' + str(end_pos) + ':Del'
-			pos = end_pos
-			alterations.append(alt)
+	# if the cigar tuple is only 1 entry check if it has a SNP
+	if len(cigar_tuple)==1:
+		for seq_pos in aligned_pairs:
+			pos = int(seq_pos[0]) + 1
+			base = seq_pos[2]
+			if base.islower():
+				alt = str(pos) + ':' + str(pos) + ':SNP'
+				alterations.append(alt)
+	else:
+		for entry in cigar_tuple:
+			# match
+			if int(entry[0])==0:
+				pos = pos + int(entry[1])
+			elif int(entry[0])==1:
+				alt = str(pos) + ':' + str(pos) + ':Ins'
+				alterations.append(alt)
+			elif int(entry[0])==2:
+				end_pos = pos + int(entry[1])
+				alt = str(pos) + ':' + str(end_pos) + ':Del'
+				pos = end_pos
+				alterations.append(alt)
 	return alterations
 
 def find_target_indices(target_site,reference_file):
@@ -77,11 +86,9 @@ def calculate_NHEJ_mutation_rate (sample_sam_file,control,reference_file,target_
 	sample_mutation_count = 0
 	control_cigar_strings = []
 
-	# write headers to the output file
-	output_file.write('File\tGene\tMutatedCount\tTotalCount\tMutation_Percentage\n')
-
 	# control bam file
-	control_sam_file = 'processed/' + control + '_bwamem_sorted.bam'
+	#control_sam_file = 'processed/' + control + '_bwamem_sorted.bam'
+	control_sam_file = control + '_bwamem_sorted.bam'
 
 	# get the start and end indexes of the target in the reference
 	target_start,target_end,gene = find_target_indices(target_site,reference_file)
@@ -91,7 +98,9 @@ def calculate_NHEJ_mutation_rate (sample_sam_file,control,reference_file,target_
 	for read in ctrl_sam.fetch():
 		if 'S' not in read.cigarstring and 'H' not in read.cigarstring and int(read.reference_start)==0:
 			# check if the read has a mutation that involves the target sequence
-			ctrl_alterations = process_cigar(read.cigartuples)
+			md_tag = read.get_tag('MD')
+			ctrl_alterations = process_cigar(read.cigartuples,md_tag,read.get_aligned_pairs(with_seq=True))
+			unique_entry = md_tag + '_' + read.cigarstring
 			# if alteration is non zero
 			valid_alteration = False
 			if len(ctrl_alterations) > 0:
@@ -100,18 +109,20 @@ def calculate_NHEJ_mutation_rate (sample_sam_file,control,reference_file,target_
 					if ((int(start) >= target_start and int(start) <= target_end) or (int(end) >= target_start and int(end) <= target_end)):
 						valid_alteration = True
 			# check if valid alteration
-			if valid_alteration==True and read.cigarstring not in control_cigar_strings:
+			if valid_alteration==True and unique_entry not in control_cigar_strings:
 				# add to the control
-				control_cigar_strings.append(read.cigarstring)
+				control_cigar_strings.append(unique_entry)
 
 	# go through sample BAM file
 	samfile = pysam.AlignmentFile(sample_sam_file,"rb")
 	for read in samfile.fetch():
 		if 'S' not in read.cigarstring and 'H' not in read.cigarstring and int(read.reference_start)==0:
 			sample_read_total += 1
-			sample_alterations = process_cigar(read.cigartuples)
+			md_tag = read.get_tag('MD')
+			sample_alterations = process_cigar(read.cigartuples,md_tag,read.get_aligned_pairs(with_seq=True))
+			unique_entry = md_tag + '_' + read.cigarstring
 			# only count if the tuple is not in the control
-			if len(sample_alterations) > 0 and read.cigarstring not in control_cigar_strings:
+			if len(sample_alterations) > 0 and unique_entry not in control_cigar_strings:
 				# if alteration is non zero
 				valid_alteration = False
 				for alt in sample_alterations:
@@ -119,9 +130,6 @@ def calculate_NHEJ_mutation_rate (sample_sam_file,control,reference_file,target_
 					if ((int(start) >= target_start and int(start) <= target_end) or (int(end) >= target_start and int(end) <= target_end)):
 						valid_alteration = True
 				if valid_alteration==True:
-					#print('Cigarstring: ' + read.cigarstring)
-					#print(str(target_start) + ',' + str(target_end))
-					#print(sample_alterations)
 					sample_mutation_count += 1
 	if sample_read_total >= 100:
 		rate = (sample_mutation_count / sample_read_total) * 100
