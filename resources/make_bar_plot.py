12033f7f70c0a5f74ae641c3f1ef3ec4ac4bda11
Raj Chari <raj.chari@nih.gov>
2019-09-01 01:41:37 -0400

    Updated bar plot to make diversity plot

--- resources/make_bar_plot.py
+++ resources/make_bar_plot.py
@@ -54,6 +54,74 @@ def make_indel_rate_plot(input_file,output_file):
 	# save to file
 	plt.savefig(output_file)
 
+def make_diversity_plot(diversity_summary_file,diversity_graph_output):
+	# read in the data
+	x_data_labels = []
+	y_data = []
+	line_count = 0
+	deletion_data = defaultdict(float)
+	insertion_data = defaultdict(float)
+	total_data = defaultdict(float)
+	mut_data = defaultdict(str)
+	for line in diversity_summary_file:
+		if line_count > 0:
+			line = line.rstrip('\r\n')
+			parts = line.split('\t')
+			del_rate = 0.0
+			ins_rate = 0.0
+			total_rate = 0.0
+			mut_data[parts[0]] = parts[3]
+			if int(parts[3]) != 0:
+				del_rate = int(parts[4]) / int(parts[3])
+				ins_rate = int(parts[5]) / int(parts[3])
+				total_rate = int(parts[6]) / int(parts[3])
+			# set the values
+			deletion_data[parts[0]] = del_rate
+			insertion_data[parts[0]] = ins_rate
+			total_data[parts[0]] = total_rate
+		line_count += 1
+	diversity_summary_file.close()
+
+	# add into lists sorted by key
+	x_labels = []
+	del_array = []
+	ins_array = []
+	total_array = []
+	for key in sorted(deletion_data.keys()):
+		name = key + '_' + mut_data[key]
+		x_labels.append(name)
+		del_array.append(deletion_data[key])
+		ins_array.append(insertion_data[key])
+		total_array.append(total_data[key])
+
+	# plotting
+	bar_width = 1
+
+	r1 = np.arange(len(x_labels)) * 4
+	r2 = r1 + bar_width
+	r3 = r2 + bar_width
+
+	print(r1)
+	print(r2)
+	print(r3)
+
+	# add axis labels
+	fig, ax = plt.subplots()
+
+	plt.bar(r1, del_array, color='k', width=bar_width, label='Deletions')
+	plt.bar(r2, ins_array, color='m', width=bar_width, label='Insertions')
+	plt.bar(r3, total_array, color='b', width=bar_width, label='Total')
+
+	plt.xlabel('Guide RNA Candidate - # of mutated reads')
+	plt.ylabel('Diversity proportion')
+	ax.set_xticks(r1+1)
+	ax.set_xticklabels(x_labels,rotation=90)
+
+	# save to file
+	ax.autoscale_view()
+	ax.legend()
+	plt.savefig(diversity_graph_output)
+
 def make_base_editing_plot(input_file,modality,output_file):
 	# only going to use position 5 to 12
 	# output_file.write('File\tGene\tTotalCount\tPos1\tPos2\tPos3\tPos4\tPos5\tPos6\tPos7\tPos8\tPos9\tPos10\tPos11\tPos12\tPos13\tPos14\tPos15\tPos16\tPos17\tPos18\tPos19\tPos20\n')
@@ -111,14 +179,17 @@ def make_base_editing_plot(input_file,modality,output_file):
 
 def main(argv):
 	parser = argparse.ArgumentParser(description=__doc__)
-	parser.add_argument('-i','--input_file',type=argparse.FileType('r'),required=True)
+	parser.add_argument('-i','--mutation_summary_file',type=argparse.FileType('r'),required=True)
+	parser.add_argument('-d','--diversity_summary_file',type=argparse.FileType('r'),required=True)
 	parser.add_argument('-m','--modality',required=True)
-	parser.add_argument('-o','--output_file',required=True)
+	parser.add_argument('-o','--mutation_graph_output',required=True)
+	parser.add_argument('-g','--diversity_graph_output',required=True)	
 	opts = parser.parse_args(argv)
 	if opts.modality=='ABE7.10' or opts.modality=='BE4':
 		make_base_editing_plot(opts.input_file,opts.modality,opts.output_file)
 	else:
-		make_indel_rate_plot(opts.input_file,opts.output_file)
+		make_indel_rate_plot(opts.mutation_summary_file,opts.mutation_graph_output)
+		make_diversity_plot(opts.diversity_summary_file,opts.diversity_graph_output)
  
 if __name__ == '__main__':
 	main(sys.argv[1:])
