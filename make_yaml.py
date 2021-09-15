90d13070db11f5bc11d2d1d2dd8e7d8ff5e3a904
rajchari2 <43412612+rajchari2@users.noreply.github.com>
2021-09-15 12:54:45 -0700

    updated make yaml

--- make_yaml.py
+++ make_yaml.py
@@ -1,7 +1,7 @@
 # script to make a yaml file from a tab-delimited sample file
 # updated 09/18/18 to compute control subtracted rates
 
-# input file: [tab-delimited - sample name {tab} reference file {tab} coordinates {tab} target site {tab} control_sample
+# input file: [tab-delimited - filename {tab} display-name {tab} reference file {tab} coordinates {tab} amplicon-name {tab} target site {tab} control_sample
 # if coordinates === full -> entire sequence
 # output file: yaml file which can run in the pipeline
 
@@ -15,6 +15,9 @@ import subprocess
 from collections import defaultdict
 
 def makeYAML (input_file,project_name,ngs_run,cell_type,modality,output_file,directory_name):
+	# distict names
+	distinct_names = defaultdict(str)
+
 	# dictionary to hold yaml
 	yaml_dict = {}
 	yaml_dict['samples'] = []
@@ -31,51 +34,74 @@ def makeYAML (input_file,project_name,ngs_run,cell_type,modality,output_file,dir
 		parts = line.split('\t')
 
 		# variables to store fields
-		sample_name = parts[0]
-		reference_file = parts[1]
-		coordinates = parts[2]
-		amplicon_name = parts[3]
-		target_site = parts[4]
-		control_sample = parts[5]
+		filename = parts[0]
+		display_name = parts[1]
+		reference_file = parts[2]
+		coordinates = parts[3]
+		amplicon_name = parts[4]
+		target_site = parts[5]
+		control_sample = parts[6]
+
+		# get the size of the amplicon
+		chrom,coords = coordinates.split(':')
+		start,end = coords.split('-')
+		size = int(end) - int(start)
+		if size <= 290:
+			trim = '150'
+		else:
+			trim = '250'
 
 		# add to control file list
 		if control_sample not in ctrl_list:
 			ctrl_list.append(control_sample)
-
-		# go through and change the filenames
-		path_to_check = directory_name + '/' + sample_name + '_*.fastq.gz'
-		file_list = sorted(glob.glob(path_to_check))
-
-		# and change each file
-		mvCommand_R1 = 'mv ' + file_list[0] + ' ' + directory_name + '/' + sample_name + '_R1.fastq.gz'
-		p = subprocess.Popen(mvCommand_R1,shell=True)
-		p.communicate()
-		mvCommand_R2 = 'mv ' + file_list[1] + ' ' + directory_name + '/' + sample_name + '_R2.fastq.gz'
-		p = subprocess.Popen(mvCommand_R2,shell=True)
-		p.communicate()
-
+		
 		# add to dictionary
 		sample_dict = {}
+		sample_name = filename + '-' + amplicon_name
+
+		# check if this is distinct
+		if sample_name not in distinct_names:
+			distinct_names[sample_name] = filename
+		else:
+			print('Sample name: ' + sample_name + ' not distinct')
+			exit()
 		sample_dict[sample_name] = {}
-		sample_dict[sample_name]['name'] = sample_name
+		sample_dict[sample_name]['display_name'] = display_name
 		sample_dict[sample_name]['reference'] = reference_file
 		sample_dict[sample_name]['coordinates'] = coordinates
 		sample_dict[sample_name]['amp_name'] = amplicon_name
 		sample_dict[sample_name]['target_site'] = target_site
 		sample_dict[sample_name]['control_sample'] = control_sample
+		sample_dict[sample_name]['trim'] = trim
 
 		# add to yaml dict
 		yaml_dict['samples'].append(sample_dict)
+
+	# now copy files
+	for sample in distinct_names:
+		filename = distinct_names[sample]
+
+		# go through and change the filenames
+		path_to_check = directory_name + '/' + filename + '_*.fastq.gz'
+		file_list = sorted(glob.glob(path_to_check))
+
+		# and change each file
+		cpCommand_R1 = 'cp ' + file_list[0] + ' ' + directory_name + '/' + sample + '_R1.fastq.gz'
+		p = subprocess.Popen(cpCommand_R1,shell=True)
+		p.communicate()
+		cpCommand_R2 = 'cp ' + file_list[1] + ' ' + directory_name + '/' + sample + '_R2.fastq.gz'
+		p = subprocess.Popen(cpCommand_R2,shell=True)
+		p.communicate()
 
 	# move the control sample files
 	for ctrl_sample in ctrl_list:
 		path_to_check = directory_name + '/' + ctrl_sample + '*.fastq.gz'
 		file_list = sorted(glob.glob(path_to_check))
 		# and change each file
-		mvCommand_R1 = 'mv ' + file_list[0] + ' ' + directory_name + '/' + ctrl_sample + '_R1.fastq.gz'
+		mvCommand_R1 = 'cp ' + file_list[0] + ' ' + directory_name + '/' + ctrl_sample + '_R1.fastq.gz'
 		p = subprocess.Popen(mvCommand_R1,shell=True)
 		p.communicate()
-		mvCommand_R2 = 'mv ' + file_list[1] + ' ' + directory_name + '/' + ctrl_sample + '_R2.fastq.gz'
+		mvCommand_R2 = 'cp ' + file_list[1] + ' ' + directory_name + '/' + ctrl_sample + '_R2.fastq.gz'
 		p = subprocess.Popen(mvCommand_R2,shell=True)
 		p.communicate()		
 
